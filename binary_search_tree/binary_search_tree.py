fd34257c53ebee560b112fd300e37f8e5ac01337
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-06-12 16:12:48 -0700

    completed insert for binary search tree and most of contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,21 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    current_node = self
+    if value >= current_node.value:
+      if current_node.right is not None:
+        current_node.right.insert(value)
+        print("insert right")
+      else:
+        current_node.right = BinarySearchTree(value)
+        return "Done"
+    else:
+      if current_node.left is not None:
+        print("insert left")
+        current_node.left.insert(value)
+      else:
+        current_node.left = BinarySearchTree(value)
+        return "Done"
 
   def contains(self, target):
     pass
@@ -15,3 +29,15 @@ class BinarySearchTree:
 
   def for_each(self, cb):
     pass
+
+tree = BinarySearchTree(5)
+print(tree.value)
+
+tree.insert(3)
+print(tree.left.value)
+tree.insert(8)
+print(tree.right.value)
+tree.insert(4)
+print(tree.left.right.value)
+tree.insert(6)
+print(tree.right.left.value)
